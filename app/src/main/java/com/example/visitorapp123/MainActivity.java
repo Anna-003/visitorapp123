7a0aabf7b49ec17bcf5c7dd9a8d552685519d3a1
anna-003 <firstphoneofanna@gmail.com>
2024-12-18 16:14:24 +0530

    feature/apicall

--- app/src/main/java/com/example/visitorapp123/MainActivity.java
+++ app/src/main/java/com/example/visitorapp123/MainActivity.java
@@ -1,24 +1,21 @@
-package com.example.visitorapp123;
+package com.example.visitorapp;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import...
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        b1=(Button)findViewById(R.id.add);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ab =new Intent(getApplicationContext(), Addvisitor.class)
+                startActivity(ab);
+            }
         });
     }
 }
